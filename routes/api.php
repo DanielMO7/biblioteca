11d6ee9db25a8b34b6e54ee752198687ddb1fdf7
DanielMO7 <dani019jorje@gmail.com>
2023-02-14 22:12:02 -0500

    Rutas para servicios del home

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('insertar', [UserController::class, 'insertar']);
 Route::post('validar-datos-registro', [UserController::class, 'validar_datos_registro']);
 Route::post('ingresar', [AuthController::class, 'ingresar']);
+
+Route::controller(HomeController::class)->prefix('home')->group(function () {
+    Route::get('frases-home', 'frases_home');
+});
 
 Route::middleware('auth:sanctum')->controller(AuthController::class)->group(function () {
     Route::post('cerrar-sesion', 'cerrar_sesion');
